330bb59d6d0d65a90a4dd4cc91e29818214bdbaa
Calvinfs852 <calvinfs852@gmail.com>
2014-06-04 14:32:50 -0400

    Razorengine
    While the result still does no compress into a valid .odt document, the
    use of RazorEngine is easy.

--- DataInjector/DataTools.cs
+++ DataInjector/DataTools.cs
@@ -2,17 +2,17 @@
 using System.Globalization;
 using System.IO;
 using System.Text;
-
+using RazorEngine;
 namespace DataInjector
 {
     internal class DataTools
     {
         public static string InsertDate(string content)
         {
+            string template = "@Model.Name";
+            var model = new {Date = DateTime.Today};
+            return Razor.Parse(template, model);
 
-            var newContent =content.Replace("@today", DateTime.Today.Date.ToString(CultureInfo.InvariantCulture));
-
-            return newContent;
 
         }
     }
