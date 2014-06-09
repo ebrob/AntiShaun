d56186a6b1591002c63ec18aa71aa970aa316e5d
Calvinfs852 <calvinfs852@gmail.com>
2014-06-09 11:10:34 -0400

    Functioning version!
    IT LIVES

--- DataInjector/DataTools.cs
+++ DataInjector/DataTools.cs
@@ -3,17 +3,47 @@ using System.Globalization;
 using System.IO;
 using System.Text;
 using RazorEngine;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Linq;
+using Encoding = System.Text.Encoding;
+
+
 namespace DataInjector
 {
     internal class DataTools
     {
-        public static string InsertDate(string content)
+        public static String InsertDate(Stream content)
         {
-            string template = "@Model.Name";
-            var model = new {Date = DateTime.Today};
-            return Razor.Parse(template, model);
+            var transformReader = new StreamReader(content);
+            var contentString = transformReader.ReadToEnd();
+            var preparedContent = contentString.Replace("@", "BLURGTWONK");
+            var preparedBytes = Encoding.UTF8.GetBytes(preparedContent);
+            var preparedStream = new MemoryStream(preparedBytes);
 
+            //TODO: Redo with XDocument?
 
+            var n = new NameTable();
+            var nsm = new XmlNamespaceManager(n);
+            nsm.AddNamespace("text", "urn:oasis:names:tc:opendocument:xmlns:text:1.0");
+            var document = new XmlDocument();
+            document.Load(preparedStream);
+
+            XmlNodeList nodes = document.SelectNodes(@"//text:text-input[ @text:description = 'Template']", nsm);
+
+            if (nodes != null)
+                foreach (XmlNode xmlNode in nodes)
+                {
+                    var flagAttr = xmlNode.InnerText; // .Attributes["text:description"]; //, "urn:oasis:names:tc:opendocument:xmlns:text:1.0"];
+                    var preparedFlag = flagAttr.Replace("BLURGTWONK", "@");
+                    //If our node has no parent then something is wrong enough to warrant throwing an exception.
+                    var newNode = document.CreateTextNode(preparedFlag);
+                    //xmlNode.ParentNode.AppendChild(newNode);
+                    xmlNode.ParentNode.ReplaceChild(newNode, xmlNode);
+                }
+            var model = new {Date = DateTime.Today, Name = "Fancypants McSnooterson"};
+            string razorOutput = Razor.Parse(document.OuterXml, model);
+            return razorOutput.Replace("BLURGTWONK", "@");
         }
     }
 }
--- DataInjector/ReportBuilder.cs
+++ DataInjector/ReportBuilder.cs
@@ -2,6 +2,11 @@
 using System.Globalization;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
+using Ionic.Zip;
+using System.IO;
+using ZipFile = Ionic.Zip.ZipFile;
+
 
 namespace DataInjector
 {
@@ -13,29 +18,71 @@ namespace DataInjector
             var folderPath = PathTools.RemoveFileNameFromPath(path);
 
             var tempFilePath = PathTools.TempFolderMaker(folderPath);
+
+
             CompressionTools.UnzipToDirectory(path, tempFilePath);
             var contentPath = tempFilePath + "\\content.xml";
             string xmlContent;
             using (var contentStream = new FileStream(contentPath, FileMode.Open))
             {
-                StreamReader contentReader = new StreamReader(contentStream);
-                string contentString = contentReader.ReadToEnd();
-                xmlContent = DataTools.InsertDate(contentString);
+                xmlContent = DataTools.InsertDate(contentStream);
             }
 
 
             File.WriteAllText(tempFilePath + "\\content.xml", xmlContent);
-            string reportsFolderPath = tempFilePath.Substring(0,
-                                                              tempFilePath.LastIndexOf("\\", StringComparison.Ordinal));
-            if (PermissionChecker.HasWritePermission(reportsFolderPath))
+            var reportsFolderPath = tempFilePath.Substring(0,
+                                                           tempFilePath.LastIndexOf("\\", StringComparison.Ordinal));
+
+            var fileList = Directory.EnumerateFiles(tempFilePath);
+            var reportPath = reportsFolderPath + "\\Report_1.odt";
+            using (var output = new ZipOutputStream(reportPath))
             {
-                var filePath = reportsFolderPath + "\\Report_1.zip";
-                ZipFile.CreateFromDirectory(tempFilePath,filePath );
+                var enumerable = fileList as string[] ?? fileList.ToArray();
+                foreach (var filepath in enumerable.Where(filepath => filepath.EndsWith("mimetype")))
+                {
+                    using (var fs = new FileStream(filepath, FileMode.Open))
+                    {
+                        var entry = output.PutNextEntry(
+                            filepath.Substring(filepath.LastIndexOf("\\", StringComparison.Ordinal)));
+                        entry.CompressionMethod = CompressionMethod.None;
+                        WriteExistingFile(fs, output);
+                    }
+                }
+                foreach (var file in enumerable)
+                {
+                    using (var fs = new FileStream(file, FileMode.Open))
+                    {
+                        if (file.EndsWith("mimetype")) continue;
+                        var entryString = file.Substring(file.LastIndexOf("\\", StringComparison.Ordinal));
+                        var entry = output.PutNextEntry(entryString);
+
+                        WriteExistingFile(fs, output);
+                    }
+                }
             }
-            
+            var zippedFile = new ZipFile(reportPath);
+            var folders = new string[] {tempFilePath +"\\Configurations2", tempFilePath+ "\\META-INF",tempFilePath+ "\\Thumbnails"};
+            foreach (var folder in folders)
+            {
+                zippedFile.AddDirectory(folder, folder.Substring(folder.LastIndexOf("\\", StringComparison.Ordinal)));
+            }
+            zippedFile.Save();
+            zippedFile.Dispose();
+
+
 
             Directory.Delete(tempFilePath, true);
             return folderPath;
+        }
+
+        private static void WriteExistingFile(Stream input, ZipOutputStream output)
+        {
+            int n = -1;
+            var buffer = new byte[2048];
+            while ((n = input.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                output.Write(buffer, 0, n);
+            }
         }
     }
 }
