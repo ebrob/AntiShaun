2d4456a84615358038512c294fd328627ed8fede
Calvinfs852 <calvinfs852@gmail.com>
2014-06-27 11:17:18 -0400

    Rip out
    Rebuilding the outward-facing parts of the project from the ground up,
    with a different paradigm.

---
+++ DataInjector/DocumentService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AODL.Document.TextDocuments;
+using EnsureThat;
+using AODL.Document;
+using AODL;
+
+namespace DataInjector
+{
+    public interface IDocumentService
+    {
+        Stream WrapBytesInStream(byte[] buffer);
+        ZipArchive ReadStreamAsArchive(Stream stream);
+        ZipArchiveEntry GetArchiveEntry(ZipArchive archive, string entryName);
+        Stream OpenArchiveEntry(ZipArchiveEntry entry);
+        string ReadEntryStreamContentAsString(Stream entry);
+    }
+
+    public class DocumentService : IDocumentService
+    {
+
+       public Stream WrapBytesInStream(byte[] buffer)
+       {
+           var stream = new MemoryStream(buffer);
+           return stream;
+       }
+
+        public ZipArchive ReadStreamAsArchive(Stream stream)
+        {
+            var archive = new ZipArchive(stream);
+            return archive;
+        }
+        
+        public ZipArchiveEntry GetArchiveEntry(ZipArchive archive, string entryName)
+        {
+            var entry = archive.GetEntry(entryName);
+            return entry;
+        }
+
+        public Stream OpenArchiveEntry(ZipArchiveEntry entry)
+        {
+            return entry.Open();
+        }
+
+        public string ReadEntryStreamContentAsString(Stream entry)
+        {
+            using (var reader = new StreamReader(entry))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+
+
+    }
+}
--- DataInjector/ReportBuilderService.cs
+++ DataInjector/ReportBuilderService.cs
@@ -1,32 +1,16 @@
-﻿using EnsureThat;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DataInjector
 {
-    public class ReportBuilderService
+    class ReportBuilderService
     {
-        private readonly IDocumentDeconstructService _deconstructService;
-        private readonly IReportTemplateService _templateService;
-        private readonly IDocumentReconstructService _reconstructService;
+        private ReportTemplateService _reportTemplateService;
+        private IDocumentService _documentService;
+        public ReportBuilderService(ReportTemplateService reportTemplateService, IDocumentService documentService)
 
-        public ReportBuilderService(IDocumentDeconstructService deconstructService,
-                                    IReportTemplateService templateService,
-                                    IDocumentReconstructService reconstructService)
-        {
-            _deconstructService = deconstructService;
-            _templateService = templateService;
-            _reconstructService = reconstructService;
-        }
-
-        public string InjectData(string pathToTemplate, object model)
-        {//TODO: Refactor to pass zip archive through, so we don't need a temp folder.
-            Ensure.That(pathToTemplate).IsNotNullOrWhiteSpace();
-            var templateContent = _deconstructService.UnzipAndGetContent(pathToTemplate);
-            
-            var xmlContent = _templateService.ApplyTemplate(templateContent, model);
-
-            return _reconstructService.Reconstruct(
-                _deconstructService.TempFolderPath,
-                xmlContent);
-        }
     }
 }
