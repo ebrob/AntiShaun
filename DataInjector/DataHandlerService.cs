d17de2d3525e3de1ab5a826caaf82131b47b08e2
Calvinfs852 <calvinfs852@gmail.com>
2014-07-02 10:06:40 -0400

    Working again
    Haven't been committing until I had it working with the new structure.
    Woo!

---
+++ DataInjector/DataHandlerService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace DataInjector
+{
+    public interface IDataHandlerService
+    {
+        Stream WrapBytesInStream(byte[] buffer);
+        ZipArchive ZipArchiveFromStream(Stream stream);
+        ZipArchiveEntry GetZipEntry(ZipArchive archive, string entryName);
+        Stream OpenZipEntry(ZipArchiveEntry entry);
+        string ReadEntryStreamContentAsString(Stream entry);
+        Byte[] Clone(byte[] original);
+    }
+
+    public class DataHandlerService : IDataHandlerService
+    {
+        //TODO: Fluent interface?
+
+        public Stream WrapBytesInStream(byte[] buffer)
+        {
+            var stream = new MemoryStream(buffer);
+            return stream;
+        }
+
+        public ZipArchive ZipArchiveFromStream(Stream stream)
+        {
+            var archive = new ZipArchive(stream, ZipArchiveMode.Update, true);
+
+            return archive;
+        }
+
+
+        public ZipArchiveEntry GetZipEntry(ZipArchive archive, string entryName)
+        {
+            var entry = archive.GetEntry(entryName);
+            return entry;
+        }
+
+        public Stream OpenZipEntry(ZipArchiveEntry entry)
+        {
+            var stream = entry.Open();
+            return stream;
+        }
+
+        public string ReadEntryStreamContentAsString(Stream entry)
+        {
+            using (var reader = new StreamReader(entry))
+            {
+                
+                string contentString = reader.ReadToEnd();
+                return contentString;
+            }
+        }
+
+        public Byte[] Clone(byte[] original)
+        {
+            var newByteArray = new byte[original.Length];
+            Buffer.BlockCopy(original, 0, newByteArray, 0, Buffer.ByteLength(original));
+            return newByteArray;
+        }
+    }
+}
--- DataInjector/ReportBuilderService.cs
+++ DataInjector/ReportBuilderService.cs
@@ -1,16 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.IO.Compression;
 
 namespace DataInjector
 {
-    class ReportBuilderService
+    public class ReportBuilderService
     {
-        private ReportTemplateService _reportTemplateService;
-        private IDocumentService _documentService;
-        public ReportBuilderService(ReportTemplateService reportTemplateService, IDocumentService documentService)
+        private readonly ReportTemplateService _reportTemplateService;
+        private readonly IDataHandlerService _dataHandlerService;
 
+        public ReportBuilderService(ReportTemplateService reportTemplateService, IDataHandlerService dataHandlerService)
+        {
+            _reportTemplateService = reportTemplateService;
+            _dataHandlerService = dataHandlerService;
+        }
+
+        public void BuildReport(byte[] templateDocument, object model, Stream outputStream)
+        {
+            string reportXml;
+
+            using (var templateFileStream = _dataHandlerService.WrapBytesInStream(templateDocument))
+            {
+                var zipFile = _dataHandlerService.ZipArchiveFromStream(templateFileStream);
+
+                var templateContentEntry = _dataHandlerService.GetZipEntry(zipFile, "content.xml");
+
+                using (var templateContentStream = _dataHandlerService.OpenZipEntry(templateContentEntry))
+                {
+                    var templateContent = _dataHandlerService.ReadEntryStreamContentAsString(templateContentStream);
+
+                    reportXml = _reportTemplateService.ApplyTemplate(templateContent, model);
+                }
+            }
+            var reportDocument = _dataHandlerService.Clone(templateDocument);
+            //BUG: This document never changes! The document isn't being edited at all!
+
+            using (var memoryStream = _dataHandlerService.WrapBytesInStream(reportDocument))
+            {
+                using (var archive = _dataHandlerService.ZipArchiveFromStream(memoryStream))
+                {
+                    var oldEntry = _dataHandlerService.GetZipEntry(archive, "content.xml");
+                    oldEntry.Delete();
+
+                    var newEntry = archive.CreateEntry("content.xml");
+                    using (var entryStream = newEntry.Open())
+                    {
+                        using (var streamWriter = new StreamWriter(entryStream))
+                        {
+                            streamWriter.Write(reportXml);
+                        }
+                    }
+                }
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                memoryStream.CopyTo(outputStream);
+            }
+           
+        }
     }
 }
